aabeeb86050397527ab3247d121364c7ec0ea141
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-05-24 12:33:46 -0400

    Remove debug code

--- src/FSharp.Control.Websockets.TPL/FSharp.Control.Websockets.TPL.fs
+++ src/FSharp.Control.Websockets.TPL/FSharp.Control.Websockets.TPL.fs
@@ -94,11 +94,6 @@ module WebSocket =
 
     let receiveMessageAsUTF8 cancellationToken socket = task {
         use stream =  new IO.MemoryStream()
-        // if not(System.Diagnostics.Debugger.IsAttached) then
-        //   printfn "Please attach a debugger, PID: %d" (System.Diagnostics.Process.GetCurrentProcess().Id)
-        // while not(System.Diagnostics.Debugger.IsAttached) do
-        //   System.Threading.Thread.Sleep(100)
-        // System.Diagnostics.Debugger.Break()
         let! result = receiveMessage cancellationToken DefaultBufferSize WebSocketMessageType.Text stream socket
         match result with
         | ReceiveStreamResult.Stream s ->
