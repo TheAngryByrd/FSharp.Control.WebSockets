b8082b2de00c4f2fa045cd6a49671eb301dfe4e7
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-05-24 13:31:09 -0400

    Bump version to 0.2.0 * BREAKING: API Changes * FEATURE: Doc Comments for all public functions

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,5 +1,9 @@
+#### 0.2.0 - 2019-05-24
+* BREAKING: API Changes
+* FEATURE: Doc Comments for all public functions
+
 #### 0.1.2 - 2019-05-23
-* Update Nuget Description
+* MINOR: Update Nuget Description
 
 #### 0.1.1 - 2019-05-23
-* Initial release
+* FEATURE: Initial release
--- src/FSharp.Control.Websockets/AssemblyInfo.fs
+++ src/FSharp.Control.Websockets/AssemblyInfo.fs
@@ -4,20 +4,20 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("FSharp.Control.Websockets")>]
 [<assembly: AssemblyProductAttribute("FSharp.Control.Websockets")>]
-[<assembly: AssemblyVersionAttribute("0.1.2")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2019-05-23T00:00:00.0000000")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.2")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.2")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2019-05-24T00:00:00.0000000")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.2.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","fb87fe1690e778194917d7778aedc1cea557d8b0")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","a1e88136d97b12a3b87a24207e56abdfcd3d9ad8")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Control.Websockets"
     let [<Literal>] AssemblyProduct = "FSharp.Control.Websockets"
-    let [<Literal>] AssemblyVersion = "0.1.2"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2019-05-23T00:00:00.0000000"
-    let [<Literal>] AssemblyFileVersion = "0.1.2"
-    let [<Literal>] AssemblyInformationalVersion = "0.1.2"
+    let [<Literal>] AssemblyVersion = "0.2.0"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2019-05-24T00:00:00.0000000"
+    let [<Literal>] AssemblyFileVersion = "0.2.0"
+    let [<Literal>] AssemblyInformationalVersion = "0.2.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "fb87fe1690e778194917d7778aedc1cea557d8b0"
+    let [<Literal>] AssemblyMetadata_GitHash = "a1e88136d97b12a3b87a24207e56abdfcd3d9ad8"
