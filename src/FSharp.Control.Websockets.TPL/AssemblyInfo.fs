ee6554c8b43597de46f7e7415ab409f242e9c715
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2019-05-24 00:00:07 -0400

    Bump version to 0.1.0 * Initial release

--- src/FSharp.Control.Websockets.TPL/AssemblyInfo.fs
+++ src/FSharp.Control.Websockets.TPL/AssemblyInfo.fs
@@ -2,22 +2,22 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyTitleAttribute("FSharp.Control.Websockets")>]
+[<assembly: AssemblyTitleAttribute("FSharp.Control.Websockets.TPL")>]
 [<assembly: AssemblyProductAttribute("FSharp.Control.Websockets")>]
 [<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2017-03-17T00:00:00.0000000")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2019-05-23T00:00:00.0000000")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","bb8964b54bee133e9af64d316dc2cfee16df7f72")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","352c204e0a495e4bdede90b1c87f7c4e849d7378")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] AssemblyTitle = "FSharp.Control.Websockets"
+    let [<Literal>] AssemblyTitle = "FSharp.Control.Websockets.TPL"
     let [<Literal>] AssemblyProduct = "FSharp.Control.Websockets"
     let [<Literal>] AssemblyVersion = "0.1.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2017-03-17T00:00:00.0000000"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2019-05-23T00:00:00.0000000"
     let [<Literal>] AssemblyFileVersion = "0.1.0"
     let [<Literal>] AssemblyInformationalVersion = "0.1.0"
     let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
-    let [<Literal>] AssemblyMetadata_GitHash = "bb8964b54bee133e9af64d316dc2cfee16df7f72"
+    let [<Literal>] AssemblyMetadata_GitHash = "352c204e0a495e4bdede90b1c87f7c4e849d7378"
