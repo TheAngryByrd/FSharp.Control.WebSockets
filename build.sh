8493c57e4544dccdf14ce0ffc9516b99ba1b133e
Jimmy Byrd <jimmybyrd@gmail.com>
2022-09-17 19:46:18 -0400

    Fix build project for CI

--- build.sh
+++ build.sh
@@ -3,34 +3,7 @@
 set -eu
 set -o pipefail
 
-# liberated from https://stackoverflow.com/a/18443300/433393
-realpath() {
-  OURPWD=$PWD
-  cd "$(dirname "$1")"
-  LINK=$(readlink "$(basename "$1")")
-  while [ "$LINK" ]; do
-    cd "$(dirname "$LINK")"
-    LINK=$(readlink "$(basename "$1")")
-  done
-  REALPATH="$PWD/$(basename "$1")"
-  cd "$OURPWD"
-  echo "$REALPATH"
-}
+echo "Restoring dotnet tools..."
+dotnet tool restore
 
-FAKE_TOOL_PATH=$(realpath .fake)
-FAKE="$FAKE_TOOL_PATH"/fake
-
-if ! [ -e "$FAKE" ]
-then
-  dotnet tool install fake-cli --tool-path "$FAKE_TOOL_PATH"
-fi
-
-PAKET_TOOL_PATH=$(realpath .paket)
-PAKET="$PAKET_TOOL_PATH"/paket
-
-if ! [ -e "$PAKET" ]
-then
-  dotnet tool install paket --tool-path "$PAKET_TOOL_PATH"
-fi
-
-FAKE_DETAILED_ERRORS=true "$FAKE" build -t "$@"
+FAKE_DETAILED_ERRORS=true dotnet run --project ./build/build.fsproj -- -t "$@"
---
+++ docsSrc/Tutorials/Getting_Started.md
@@ -0,0 +1,15 @@
+# Getting Started
+
+```fsharp
+open FSharp.Control.WebSockets
+let foo = ()
+let myAge = 21
+let color = Say.FavoriteColor.Red
+```
+
+## Here is the path to downloading
+
+    [lang=bash]
+    paket install FSharp.Control.WebSockets
+
+
