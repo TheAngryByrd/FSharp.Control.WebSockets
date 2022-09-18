88eedc80513219e7e9c56fbcbc51e5e79ba7ec97
Jimmy Byrd <jimmybyrd@gmail.com>
2022-09-17 20:31:10 -0400

    Bump version to 0.2.3
    ## [0.2.3] - 2022-09-17
    
    ### Changed
    - Using native tasks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,11 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.2.3] - 2022-09-17
+
+### Changed
+- Using native tasks
+
 ## [0.2.3-beta001] - 2022-09-17
 
 ### Changed
@@ -41,6 +46,7 @@ First release
 ### Added
 - Initial Release
 
-[Unreleased]: https://github.com/TheAngryByrd/FSharp.Control.WebSockets/compare/v0.2.3-beta001...HEAD
+[Unreleased]: https://github.com/TheAngryByrd/FSharp.Control.WebSockets/compare/v0.2.3...HEAD
+[0.2.3]: https://github.com/TheAngryByrd/FSharp.Control.WebSockets/compare/v0.2.2...v0.2.3
 [0.2.3-beta001]: https://github.com/TheAngryByrd/FSharp.Control.WebSockets/compare/v0.2.2...v0.2.3-beta001
 [0.1.0]: https://github.com/user/MyCoolNewLib.git/releases/tag/v0.1.0
--- src/FSharp.Control.Websockets.TPL/AssemblyInfo.fs
+++ src/FSharp.Control.Websockets.TPL/AssemblyInfo.fs
@@ -8,8 +8,8 @@ open System.Reflection
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-09-17T00:00:00.0000000-04:00")>]
 [<assembly: AssemblyFileVersionAttribute("0.2.3")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.2.3")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","beta")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","c9ad097b9bde148b8beba0676a8bc6447c8e097e")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","da52e1fd950e50d3ffe499a21fcdc67c49421413")>]
 do ()
 
 module internal AssemblyVersionInformation =
@@ -19,5 +19,5 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-09-17T00:00:00.0000000-04:00"
     let [<Literal>] AssemblyFileVersion = "0.2.3"
     let [<Literal>] AssemblyInformationalVersion = "0.2.3"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "beta"
-    let [<Literal>] AssemblyMetadata_GitHash = "c9ad097b9bde148b8beba0676a8bc6447c8e097e"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
+    let [<Literal>] AssemblyMetadata_GitHash = "da52e1fd950e50d3ffe499a21fcdc67c49421413"
